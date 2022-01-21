d393c23d0f0f52e7ec3fd3acb8d8169259119d91
João Vitor <joaovmachado10@gmail.com>
2022-01-21 12:00:08 -0300

    adicionado rota atualiza status do pedido e envia mensagem

--- src/LocalAPI/Rest/routes/whatsapp.js
+++ src/LocalAPI/Rest/routes/whatsapp.js
@@ -10,23 +10,23 @@ router.post("/message", authenticate, async function (req, res) {
         if (!number) return res.status(400).json({ error: "missing number" });
         if (!message) return res.status(400).json({ error: "missing message" });
         if (!global.whatsapp_number) return res.status(422).json({ error: "whatsapp not ready" });
-    
+
         const responseChannelId = String(Math.random()).replace(".", "");
         let responseChannelTimeout;
-    
+
         const responseFunction = (event, response, error) => {
             clearTimeout(responseChannelTimeout);
             if (error) res.status(500).json({ error });
             else res.sendStatus(200);
         };
-    
+
         responseChannelTimeout = setTimeout(() => {
             ipcMain.off(responseChannelId, responseFunction);
-            res.status(408).json({ error: "response channel timeout" });
+            res.status(500).json({ error: "response channel timeout" });
         }, 3000);
-    
+
         win.webContents.send("whatsapp:sendMessage", responseChannelId, number, message);
-    
+
         ipcMain.once(responseChannelId, responseFunction);
     } catch (error) {
         res.status(500).json({ error });
@@ -37,20 +37,52 @@ router.get("/order/:id_order", authenticate, async function (req, res) {
     try {
         if (!req.params.id_order || isNaN(req.params.id_order)) return res.status(400).json({ error: "missing id_order" });
         if (!global.whatsapp_number) return res.status(422).json({ error: "whatsapp not ready" });
-    
+
         const responseChannelId = String(Math.random()).replace(".", "");
-        const responseChannelTimeout = setTimeout(() => {
-            ipcMain.off(responseChannelId);
-            res.status(408).json({ error: "response channel timeout" });
-        }, 30000);
-    
-        win.webContents.send("whatsapp:sendOrderMessage", responseChannelId, req.params.id_order);
-    
-        ipcMain.once(responseChannelId, (event, response, error) => {
+        let responseChannelTimeout;
+
+        const responseFunction = (event, response, error) => {
             clearTimeout(responseChannelTimeout);
             if (error) res.status(500).json({ error });
             else res.sendStatus(200);
-        });
+        };
+
+        responseChannelTimeout = setTimeout(() => {
+            ipcMain.off(responseChannelId, responseFunction);
+            res.status(500).json({ error: "response channel timeout" });
+        }, 30000);
+
+        win.webContents.send("whatsapp:sendOrderMessage", responseChannelId, req.params.id_order);
+
+        ipcMain.once(responseChannelId, responseFunction);
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+});
+
+router.put("/order/:id_order", authenticate, async function (req, res) {
+    try {
+        if (!req.params.id_order || isNaN(req.params.id_order)) return res.status(400).json({ error: "missing id_order" });
+        if (!req.body.status) return res.status(400).json({ error: "missing status" });
+        if (!global.whatsapp_number) return res.status(422).json({ error: "whatsapp not ready" });
+
+        const responseChannelId = String(Math.random()).replace(".", "");
+        let responseChannelTimeout;
+
+        const responseFunction = (event, response, error) => {
+            clearTimeout(responseChannelTimeout);
+            if (error) res.status(500).json({ error });
+            else res.sendStatus(200);
+        };
+
+        responseChannelTimeout = setTimeout(() => {
+            ipcMain.off(responseChannelId, responseFunction);
+            res.status(500).json({ error: "response channel timeout" });
+        }, 30000);
+
+        win.webContents.send("whatsapp:sendOrderMessage", responseChannelId, req.params.id_order, req.body.status);
+
+        ipcMain.once(responseChannelId, responseFunction);
     } catch (error) {
         res.status(500).json({ error });
     }
